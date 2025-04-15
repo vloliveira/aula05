d44573c6c639efd30b418b76730c42ba0afca922
Vitor Oliveira <vitorloliveira96@gmail.com>
2025-04-15 09:59:37 -0300

    exercicio05

---
+++ exercicio05.py
@@ -0,0 +1,9 @@
+
+v1 = int(input("Digite o primeiro valor: "))
+v2 = int(input("Digite o segundo valor: "))
+
+while v2 <=0:
+    v2 = int(input("Digite o segundo valor: "))
+
+div = v1/v2
+print(div)
